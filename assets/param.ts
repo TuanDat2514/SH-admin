13f9f516a4e9d5de1fcb45d0c93fa10b43127342
TuanDat2514 <dattuantrinh77@gmail.com>
2023-01-30 14:11:17 +0700

    Auto-generated commit [ci skip]

---
+++ assets/param.ts
@@ -0,0 +1,21 @@
+export const URL_ROOT = "http://localhost:8080";
+
+export const PARAM = {
+  //received
+  GET_LIST_RECEIVED:"cart/getListCart",
+  UPDATE_RECEIVED:"cart/update",
+  GET_DETAIL:"shipping",
+  GET_RECEIVED_BY_ID:"cart/getCart",
+  //product
+  GET_ALL_PRODUCT:"product/all",
+  UPDATE_PRODUCT:"product/update",
+  GET_PRODUCT_BY_ID:"product/getProdbyId",
+  ADD_PRODUCT:"product/add",
+  DELETE_PRODUCT:"product/delete",
+  SEARCH_PRODUCT:"product/search?key",
+  //stock
+  GET_STOCK_PRODUCT:"stock/stockProduct",
+
+  //discount
+  GET_ALL_DISCOUNT:"discount/getAllDiscount"
+}
